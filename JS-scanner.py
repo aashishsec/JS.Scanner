4fec9822a16a9c361e7af0c2d28a2069a6706d6a
AashishSec <65489287+aashishsec@users.noreply.github.com>
2023-11-25 07:41:41 +0530

    Update JS-scanner.py to mark 5

--- JS-scanner.py
+++ JS-scanner.py
@@ -1,31 +1,48 @@
-import re
-import random
+import re,random,colorama,argparse
+
 from selenium import webdriver
+
 from selenium.common.exceptions import TimeoutException, WebDriverException
-import colorama
-import argparse
 
+from datetime import datetime
 
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
+
 colorama.init(autoreset=True)
+
 green = Fore.GREEN
+
 magenta = Fore.MAGENTA
+
 cyan = Fore.CYAN
+
 mixed = Fore.RED + Fore.BLUE
+
 red = Fore.RED
+
 blue = Fore.BLUE
+
 yellow = Fore.YELLOW
+
 white = Fore.WHITE
+
 colors = [magenta,cyan,mixed,red,blue,yellow, white]
+
 random_color = random.choice(colors)
+
 bold = Style.BRIGHT
 
-
 parser=argparse.ArgumentParser(description=f"{bold}{random_color}JSScanner is a tool designed to efficiently scan to find Sensitive Files like API keys, Emails, Phone No, URLs etc.....")
+
 parser.add_argument('-l','--list',metavar='list',type=str,required=True,help=f"[{bold}{random_color}INFO]: {bold}{random_color}List of JS URLs.")
+
+
 parser.add_argument('-o','--output',metavar='output',type=str,default="output.txt",help=f"[{bold}{random_color}INFO]: {bold}{random_color}File to save our output.")
+
 args=parser.parse_args()
+
 list=args.list
+
 output=args.output
 
 
@@ -46,6 +63,11 @@ def banner():
         Github   : https://github.com/aashis36
         
 ''')
+    print("-" * 80)
+
+    print(f"{bold}{random_color}JsScanner starting at {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
+
+    print("-" * 80)
 
 
 # Define the regex patterns
@@ -64,10 +86,9 @@ banner()
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
-
-
 # Read the file containing JS URLs
 with open(js_file_path) as f:
+
     js_urls = f.read().splitlines()
 
 output_lines = []
@@ -81,6 +102,7 @@ for js_url in js_urls:
     try:
         # Navigate to the JavaScript file
         driver.set_page_load_timeout(10)
+
         driver.get(js_url)
 
         # Retrieve the contents of the file
@@ -90,31 +112,50 @@ for js_url in js_urls:
         matches = [pattern.findall(javascript_code) for pattern in patterns.values()]
 
         if any(matches):
+
             output_lines.append("Sensitive data found:\n")
+
             print(f"{bold}{random_color}Sensitive data found:\n")
+
             for pattern_name in patterns:
+
                 pattern = patterns.get(pattern_name)
+
                 pattern_matches = pattern.findall(javascript_code)
+
                 if pattern_matches:
+
                     output_lines.append(f'{pattern_name.upper()} Found:')
+
                     print(f'{bold}{random_color}{pattern_name.upper()} Found:')
+
                     for match in pattern_matches:
+
                         output_lines.append(f'- {match}')
+
                         print(f'{bold}{random_color}- {match}')
+
                     output_lines.append('')
+
                     print('')
         else:
+
             output_lines.append("No sensitive data found.\n")
+
             print(f"{bold}{random_color}No sensitive data found.\n")
 
     except TimeoutException:
+
         output_lines.append("Timeout error occurred while loading the URL.\n")
+
     except WebDriverException:
+
         output_lines.append("WebDriver error occurred while processing the URL.\n")
 
 # Save the output to a file
-with open('output.txt', 'w') as f:
-    f.write('\n'.join(output_lines))
+with open('JsScanner.txt', 'w') as f:
 
+    f.writelines(output_lines)
+    
 # Close the driver
 driver.quit()
